a8d5d5c9f40043bf31d5b31050e1c7bd2a599337
Kevin Mehall <km@kevinmehall.net>
2014-11-07 10:36:59 -0800

    Try setting OpenGL profile and version

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QSurfaceFormat>
 #include "SMU.h"
 
 int main(int argc, char *argv[])
 {
     registerTypes();
+
+    QSurfaceFormat fmt;
+    qDebug() << "Default: " << fmt.profile() << fmt.majorVersion() << fmt.minorVersion();
+
+    fmt.setVersion(2, 0);
+    fmt.setProfile(QSurfaceFormat::NoProfile);
+
+    qDebug() << "Using: " << fmt.profile() << fmt.majorVersion() << fmt.minorVersion();
+    QSurfaceFormat::setDefaultFormat(fmt);
 
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
