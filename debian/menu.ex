af2f22edc3b136c404f583c79ad569ae3146b62c
Ian Daniher <it.daniher@gmail.com>
2015-02-22 23:26:25 -0500

    clean up build resources

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pixelpulse2):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pixelpulse2" command="/usr/bin/pixelpulse2"
--- debian/pixelpulse2.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pixelpulse2 package
-#
-0 4	* * *	root	[ -x /usr/bin/pixelpulse2_maintenance ] && /usr/bin/pixelpulse2_maintenance
