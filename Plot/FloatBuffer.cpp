a75a45aae9808b522f5d179278567cedacd353f0
ezra <ezra@indico.io>
2015-02-24 12:04:19 -0500

    4 space tabs all the way accross the sky

--- Plot/FloatBuffer.cpp
+++ Plot/FloatBuffer.cpp
@@ -1,8 +1,8 @@
 #include "FloatBuffer.h"
 
 FloatBuffer::FloatBuffer(QObject *parent):
-	QObject(parent),
-	m_secondsPerSample(0.00001),
-	m_start(0),
+    QObject(parent),
+    m_secondsPerSample(0.00001),
+    m_start(0),
     m_length(0)
-	{}
+    {}
