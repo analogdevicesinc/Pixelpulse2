8961d3c2bfd1073b8f0b0ef9a4e7e6d07ba57b13
Cristi Iacob <cristian.iacob@analog.com>
2019-09-03 14:50:10 +0300

    Added find device by led functionality.
    You can now spot each device from the GUI by clicking each of the ADALM1000 labels from the left of the screen.
    The corresponding device's leds will blink.
    
    Signed-off-by: Cristi Iacob <cristian.iacob@analog.com>

--- SMU.h
+++ SMU.h
@@ -107,6 +107,7 @@ public:
     QString getDevSN() { return QString::fromStdString(m_device->m_serial); }
     int getDefaultRate() { return m_device->get_default_rate(); }
     Q_INVOKABLE int ctrl_transfer( int x, int y, int z) { return m_device->ctrl_transfer(0x40, x, y, z, 0, 0, 100);}
+    Q_INVOKABLE void blinkLeds();
 
     size_t samplesAdded() { return m_samples_added; }
     void setSamplesAdded(size_t count) { m_samples_added = count; }
